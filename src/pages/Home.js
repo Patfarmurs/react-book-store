3c059b7b9abcaa7a28eb12e501fcb5fa4440a0ce
unknown <patrickkeita80@yahoo.com>
2023-04-04 03:25:33 -0700

    Add Home.js file

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BookList from '../components/BookList';
+import Form from '../components/Form';
+
+const Home = () => (
+  <>
+    <div>
+      <BookList />
+    </div>
+    <div>
+      <Form />
+    </div>
+  </>
+);
+
+export default Home;
