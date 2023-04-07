1d3914a472a64b25f53f4b37a555cdc4caea1a1e
unknown <patrickkeita80@yahoo.com>
2023-04-06 22:39:10 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchBooks } from './redux/books/books';
 import BookList from './components/BookList';
 import Navigation from './components/Navigation';
 import CategoryPage from './pages/CategoryPage';
 
-const App = () => (
-  <div>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<BookList />} />
-      <Route path="/category" element={<CategoryPage />} />
-    </Routes>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  dispatch(fetchBooks());
+  return (
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/category" element={<CategoryPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
